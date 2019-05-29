56ca761c6d4acebbbdd2c6c7014b593caca4ac6e
n01e0 <noleo@vanillabeans.mobi>
2019-05-29 11:25:03 +0900

    Make America Great Again

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 echo "no"
 gcc -o no no.c
 chmod a+x no
+cp no /usr/bin
 cp no /bin
--- no.c
+++ no.c
@@ -1,7 +1,26 @@
 #include <stdio.h>
-int main(){
-    for(;;){
-        printf("n\n");
+#include <string.h>
+void help(void);
+int main(int argc, char *argv[]){
+    if(argc ==1){
+        while(1){
+            printf("n\n");
+        }
+    }else if(argc == 3){
+        if(strcmp(argv[1],"-s") == 0){
+            while(1){
+                printf("%s\n",argv[2]);
+            }
+        }else if(strcmp(argv[1],"-h") == 0){
+            help();
+        }
+    }else{
+        help();
     }
     return 0;
+}
+
+void help(void){
+    printf("usage no -h             :print this help\n");
+    printf("      no -s [strings]   :print [strings] instead of 'no'\n");
 }
